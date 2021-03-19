7ed503272ca01325c4b7d206449a419e9da62280
Michael Maurer <3191110276@users.noreply.github.com>
2021-03-19 22:45:49 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,3 +11,9 @@ variable "cluster_name" {
   type        = string
   description = "Name of the cluster in Intersight. Has to be unique per Intersight instance."
 }
+
+variable "iwo_server_version" {
+  type        = string
+  default     = "8.0"
+  description = "Version of the IWO server. Default can be used in most cases."
+}
