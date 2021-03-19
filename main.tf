5b89aabc6608ae6f058d47825edf1fdb1dd9f577
Michael Maurer <3191110276@users.noreply.github.com>
2021-03-19 22:25:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 ############################################################
 terraform {
   required_providers {
-    helm = {
+    kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.2"
     }
