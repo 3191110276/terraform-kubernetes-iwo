6fefa0b07d17903ff2c4a7a2cb0096a18bfe872b
Michael Maurer <3191110276@users.noreply.github.com>
2021-03-19 22:24:44 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,10 +2,12 @@
 # INPUT VARIABLES
 ############################################################
 variable "namespace" {
-  type    = string
-  default = "iwo"
+  type        = string
+  default     = "iwo"
+  description = "Namespace used for deploying the IWO objects. This namespace has to exist and is not provisioned by this module"
 }
 
 variable "cluster_name" {
-  type    = string
+  type        = string
+  description = "Name of the cluster in Intersight. Has to be unique per Intersight instance."
 }
