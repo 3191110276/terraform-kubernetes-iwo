2fefa16fd907768596bb4fe33550322b2009e2c8
Michael Maurer <3191110276@users.noreply.github.com>
2021-03-19 22:22:34 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,11 @@
+############################################################
+# INPUT VARIABLES
+############################################################
+variable "namespace" {
+  type    = string
+  default = "iwo"
+}
 
+variable "cluster_name" {
+  type    = string
+}
