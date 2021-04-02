493c4779baa531d0425241db54050f122470c0a2
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-02 19:08:37 +0200

    Update get_iwo_info.sh

--- get_iwo_info.sh
+++ get_iwo_info.sh
@@ -2,9 +2,19 @@
 
 set -e
 
+# Extract inputs
+eval "$(jq -r '@sh "NAMESPACE=\(.namespace)"')"
+echo $NAMESPACE
+
 # Query data
-ID=kubectl -n iwo -c iwo-k8s-collector exec -it "$(kubectl get pod -n iwo | sed -n 2p | awk '{print $1}')" -- curl -s http://localhost:9110/DeviceIdentifiers | jq '.[].Id'
-TOKEN=kubectl -n iwo -c iwo-k8s-collector exec -it "$(kubectl get pod -n iwo | sed -n 2p | awk '{print $1}')" -- curl -s http://localhost:9110/SecurityTokens | jq '.[].Token'
+ID=$(kubectl -n iwo -c iwo-k8s-collector exec -it "$(kubectl get pod -n iwo | sed -n 2p | awk '{print $1}')" -- curl -s http://localhost:9110/DeviceIdentifiers | jq '.[].Id')
+TOKEN=$(kubectl -n iwo -c iwo-k8s-collector exec -it "$(kubectl get pod -n iwo | sed -n 2p | awk '{print $1}')" -- curl -s http://localhost:9110/SecurityTokens | jq '.[].Token')
+
+ID="${ID%\"}"
+ID="${ID#\"}"
+
+TOKEN="${TOKEN%\"}"
+TOKEN="${TOKEN#\"}"
 
 # Create output
-jq -n --arg id "$ID" --arg token "$TOKEN" '{"foobaz":$foobaz}'
+jq -n --arg id "$ID" --arg token "$TOKEN" '{"id":$id,"token":$token}'
