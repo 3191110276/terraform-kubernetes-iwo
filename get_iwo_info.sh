13c61a814046372c415868229418aacbc901ff72
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-02 17:29:31 +0200

    Create get_iwo_info.sh

---
+++ get_iwo_info.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+# Query data
+ID=kubectl -n iwo -c iwo-k8s-collector exec -it "$(kubectl get pod -n iwo | sed -n 2p | awk '{print $1}')" -- curl -s http://localhost:9110/DeviceIdentifiers | jq '.[].Id'
+TOKEN=kubectl -n iwo -c iwo-k8s-collector exec -it "$(kubectl get pod -n iwo | sed -n 2p | awk '{print $1}')" -- curl -s http://localhost:9110/SecurityTokens | jq '.[].Token'
+
+# Create output
+jq -n --arg id "$ID" --arg token "$TOKEN" '{"foobaz":$foobaz}'
