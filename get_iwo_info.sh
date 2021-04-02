852bfa4632480a5b160bde2206ed72c9834c3b80
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-02 19:15:52 +0200

    Update get_iwo_info.sh

--- get_iwo_info.sh
+++ get_iwo_info.sh
@@ -4,7 +4,6 @@ set -e
 
 # Extract inputs
 eval "$(jq -r '@sh "NAMESPACE=\(.namespace)"')"
-echo $NAMESPACE
 
 # Query data
 ID=$(kubectl -n iwo -c iwo-k8s-collector exec -it "$(kubectl get pod -n iwo | sed -n 2p | awk '{print $1}')" -- curl -s http://localhost:9110/DeviceIdentifiers | jq '.[].Id')
